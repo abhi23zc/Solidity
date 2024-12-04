6f84b45ef13fb7573f6f9962450d9e424c3f79e1
abhi23zc <abhishekssingh0000@gmail.com>
2024-12-05 00:56:46 +0530

    Solidity Variables

--- basic.sol
+++ basic.sol
@@ -3,4 +3,13 @@ pragma solidity >= 0.7.0;
 
 contract Basic{
     
+    bool active = true;
+    int length = 10;
+    uint length2 = 6;
+    string name = "Abhi";
+    address add = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;
+
 }
+
+
+
